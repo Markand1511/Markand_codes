d19d0f2dd17ce7e3a77dd0b8fa4f6609485ae9c0
Markand1511 <markandkacha1511@gmail.com>
2026-02-05 11:27:26 +0530

    done

--- bank.py
+++ bank.py
@@ -7,14 +7,21 @@ class bank:
     
     def deposit(self,money):
 
-        if money>=100 and money<=10000:
-            self.balance+=money
+        try:
 
-        else:
-            print("your deposit value are low")
+            if money>=100 and money<=10000:
+                self.balance+=money
+
+            else:
+                print("your deposit value are low")
+
+        except ValueError:
+
+            print("enter only numbers ")
+
 
     def withdraw(self,money):
-
+        
         if money<=self.balance and money>=100 and money<=10000:
             print('\nyour withdraw value are under your balance\n')
 
@@ -28,12 +35,16 @@ class bank:
     
 obj=bank(9558228763,25000)
 
-user=int(input('enter your deposit value : '))
-obj.deposit(user)
-print(f'\nyou just deposited {user} in your bank ')
-print('\nthis is your total bank balance after your deposit : ',obj.get_value())
+try:
+    user=float(input('enter your deposit value : '))
+    obj.deposit(user)
+    print(f'\nyou just deposited {user} in your bank ')
+    print('\nthis is your total bank balance after your deposit : ',obj.get_value())
 
-user=int(input('\nenter your withdraw value : '))
-obj.withdraw(user)
-print(f'\nyou just withdraw {user} in your bank')
-print('\nthis is your total bank balance after your withdraw : ',obj.get_value())
+    user=float(input('\nenter your withdraw value : '))
+    obj.withdraw(user)
+    print(f'\nyou just withdraw {user} in your bank')
+    print('\nthis is your total bank balance after your withdraw : ',obj.get_value())
+
+except ValueError:
+    print("type only numbers ")
